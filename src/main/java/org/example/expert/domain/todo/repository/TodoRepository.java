c4f33d9f36a8eb95fc1ac32521357858667bee0d
Han Kyobeom <ryqja18@gmail.com>
2025-01-03 14:18:05 +0900

    :adhesive_bandage: fix N+1 problem with join fetch

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -4,9 +4,14 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+  @Query("SELECT t FROM Todo t " +
+      "JOIN FETCH t.user u " +
+      "JOIN FETCH t.managers m " +
+      "ORDER BY t.modifiedAt DESC")
   Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
   int countById(Long todoId);

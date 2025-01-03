5191528e5f165f0f34e8ce418d3fe446b4d89ed0
legoChoi <chlx159@gmail.com>
2025-01-03 15:07:15 +0900

    Refactor/ lv04 N+1 problem - FETCH JOIN

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -4,10 +4,14 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+    @Query("SELECT t FROM Todo t JOIN FETCH t.user")
+    Page<Todo> findAll(Pageable pageable);
 
     int countById(Long todoId);
 }
